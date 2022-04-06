a884b6374c47f226e3985c8a520821707f69be31
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-04-06 13:05:56 -0400

    added logic to update cart item quantity

---
+++ app/src/main/java/com/test/customizelistview/ViewCartActivity.java
@@ -0,0 +1,14 @@
+package com.test.customizelistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cart);
+    }
+}
