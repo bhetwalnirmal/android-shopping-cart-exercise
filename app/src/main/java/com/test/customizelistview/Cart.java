8eff2ae91946a35322be4603230450130eed6c30
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-04-06 12:06:33 -0400

    added cart icon

---
+++ app/src/main/java/com/test/customizelistview/Cart.java
@@ -0,0 +1,31 @@
+package com.test.customizelistview;
+
+public class Cart {
+    private Product product;
+    private int quantity;
+
+    public Cart(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getProductTotal () {
+        return this.getQuantity() * this.getProduct().getPrice();
+    }
+}
--- app/src/main/java/com/test/customizelistview/MainActivity.java
+++ app/src/main/java/com/test/customizelistview/MainActivity.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     // list of items to store
     ArrayList<Product> productList = new ArrayList<Product>();
+    private ArrayList<Cart> cart = new ArrayList<Cart>();
+    Product selectedProduct;
     ListView lv;
     TextView qty, total;
     SeekBar sb;
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 currentPrice = productList.get(i).getPrice();
+                selectedProduct = productList.get(i);
             }
         });
 
@@ -59,12 +62,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
         add.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                double currentQty = Double.parseDouble(qty.getText().toString());
-                totalAmount += currentPrice * currentQty;
-                total.setText(String.format("$ %.2f", totalAmount));
+                int currentQty = Integer.parseInt(qty.getText().toString());
+                if (selectedProduct != null) {
+                    addProductToCart(selectedProduct, currentQty);
+                    updateTotal();
+                }
             }
         });
     }
@@ -76,5 +80,27 @@ public class MainActivity extends AppCompatActivity {
         productList.add(new Product("Women shirt", 53.5, R.drawable.womenshirt));
         productList.add(new Product("Women pant", 67, R.drawable.womenpant));
         productList.add(new Product("Men pant", 55, R.drawable.pantmen));
+    }
+
+    protected void addProductToCart (Product selectedProduct, int quantity) {
+        Cart newItem = new Cart(selectedProduct, quantity);
+
+        for (Cart item : cart) {
+            if (item.getProduct().getName().equals(selectedProduct.getName())) {
+                quantity = item.getQuantity() + quantity;
+                newItem.setQuantity(quantity);
+                break;
+            }
+        }
+
+        cart.add(newItem);
+    }
+
+    protected void updateTotal () {
+        double totalAmount = 0;
+        for (Cart item : cart) {
+            totalAmount += item.getProductTotal();
+        }
+        total.setText(String.format("$ %.2f", totalAmount));
     }
 }
